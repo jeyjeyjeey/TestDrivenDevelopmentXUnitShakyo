684fadb67e41dd38a88944320d2c17ee2a6b4291
horyuji <jeyjeyjeey@gmail.com>
2021-12-05 21:50:36 +0900

    20章

---
+++ kbtdd/tests/TestCaseTest.py
@@ -0,0 +1,16 @@
+from kbtdd.WasRun import WasRun
+from kbtdd.TestCase import TestCase
+
+class TestCaseTest(TestCase):
+  def setUp(self):
+    self.test = WasRun("testMethod")
+  def testRunning(self):
+    self.test.run()
+    assert(self.test.wasRun)
+  def testSetUp(self):
+    self.test.run()
+    assert(self.test.wasSetUp)
+
+
+TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
